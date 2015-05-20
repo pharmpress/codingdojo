f8bab8f8592a1e600141b8e1953f9a5082e7f398
unknown <Ramiro Calle@PC0343.rps.local>
2015-05-20 13:22:05 +0100

    tests with cucumber-jvm

--- dojo-bowling/src/main/scala/Game.scala
+++ dojo-bowling/src/main/scala/Game.scala
@@ -11,13 +11,12 @@ class Game {
   }
 
   def scoreRec(throws:Seq[Int]): Int ={
-    throws match {
-      case 10 :: y :: z :: tail => 10 + y + z + scoreRec(y :: z :: tail)
-      case x :: y :: z :: tail if x + y == 10 => 10 + z + scoreRec(z :: tail)
-      case x :: y :: tail => x + y + scoreRec(tail)
-
-      case _ => 0
-    }
+      throws match {
+        case 10 :: (tail @ y :: z :: rest) => 10 + y + z + (if (rest == Nil) 0 else scoreRec(tail))
+        case x :: y :: (tail @ z :: rest) if x + y == 10 => 10 + z + (if (rest == Nil) 0 else scoreRec(tail))
+        case x :: y :: tail => x + y + scoreRec(tail)
+        case _ => 0
+      }
   }
 
   def roll(i: Int) = throws = throws.:+(i)
--- dojo-bowling/src/test/scala/dojo/bowling/BowlingSteps.scala
+++ dojo-bowling/src/test/scala/dojo/bowling/BowlingSteps.scala
@@ -1,11 +1,54 @@
 package dojo.bowling
 
 
-import cucumber.api.DataTable
+import cucumber.api.{PendingException, DataTable}
 import cucumber.api.scala.{EN, ScalaDsl}
-import org.junit.Assert._
+import org. junit.Assert._
 import scala.collection.JavaConversions.asScalaBuffer
 
 class BowlingSteps extends ScalaDsl with EN {
+  var game: Game = _
+  Given("""^a new bowling game$"""){ () =>
+    game = new Game
+  }
 
+  When("""^all of my balls are landing in the gutter$"""){ () =>
+    for (i <- 0 until 20)
+      game.roll(0)
+  }
+  Then("""^my total score should be (\d+)$"""){ (arg0:Int) =>
+    assertEquals(arg0, game.score)
+  }
+  When("""^all of my rolls are strikes$"""){ () =>
+    for (i <- 0 until 12)
+      game.roll(10)
+  }
+  When("""^I roll (\d+) and (\d+)$"""){ (arg0:Int, arg1:Int) =>
+    game.roll(arg0)
+    game.roll(arg1)
+  }
+  When("""^I roll (\d+) and (\d+), (\d+) times$"""){ (arg0:Int, arg1:Int, arg2:Int) =>
+    for (i <- 0 until arg2) {
+      game.roll(arg0)
+      game.roll(arg1)
+    }
+  }
+  When("""^I roll the following series:$"""){ (arg0:DataTable) =>
+    arg0.transpose().asList[Integer](classOf[Integer]).foreach{ game.roll(_) }
+  }
+
+  When("""^I roll the following formatted series:$"""){ (arg0:DataTable) =>
+    arg0.transpose().asList[String](classOf[String]).foreach{ case guff =>
+      val arr = guff.split("\\s")
+      game.roll(Integer.parseInt(arr(0)))
+      game.roll(Integer.parseInt(arr(1)))
+    }
+  }
+  Then("""^my score table should be$"""){ (arg0:DataTable) =>
+
+  }
+  When("""^I roll (\d+)$"""){ (arg0:Int) =>
+    //// Write code here that turns the phrase above into concrete actions
+    throw new PendingException()
+  }
 }
